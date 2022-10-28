2e2d6ade20765f303dd5bd091f94e18fbbaef9f4
Pranav R <pranav81234@gmail.com>
2022-10-28 12:46:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Cart from "./Components/Cart/Cart";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const showCartHandler = () => {
+        setCartIsShown(true);
+    };
+
+    const hideCartHandler = () => {
+        setCartIsShown(false);
+    };
+
+    return (
+        <React.Fragment>
+            {cartIsShown && <Cart onClose={hideCartHandler}/>}
+            <Header onShowCart={showCartHandler}/>
+            <main>
+                <Meals />
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton onClick={props.onShowCart}/>
+                {/* <button>Cart</button> */}
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="ReactMeals Header" />
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Header;
